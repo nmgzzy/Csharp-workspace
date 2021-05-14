ec99bdea399312935e4ccd103a8c2c9f8405afef
nmgzzy <283003556@qq.com>
2021-05-14 23:03:09 +0800

    5.14

---
+++ GraphicsPractice/GraphicsPractice/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace GraphicsPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 999;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(858, 761);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(858, 761);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Clock";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ GraphicsPractice/GraphicsPractice/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsPractice
+{
+    public partial class Form1 : Form
+    {
+        private int mode = 0;
+        private const int modemax = 6;
+        public Form1()
+        {
+            InitializeComponent();
+            //this.SetStyle(ControlStyles.UserPaint, true);
+            //SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            //SetStyle(ControlStyles.DoubleBuffer, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private int fromString(string s)
+        {
+            return (int)(Convert.ToInt32(s, 16) | 0xff000000);
+        }
+        private PointF fromRtheta(PointF start, float r, float theta)
+        {
+            PointF pf = new PointF();
+            pf.X = start.X + r * (float)Math.Sin(theta * Math.PI / 180);
+            pf.Y = start.Y - r * (float)Math.Cos(theta * Math.PI / 180);
+            return pf;
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            PointF lefttop = new PointF(0, 0);
+            float len = Math.Min(this.panel1.Size.Width, this.panel1.Size.Height);
+            PointF pcenter = new PointF(len / 2, len / 2);
+            SizeF size = new SizeF(len, len);
+            RectangleF rect = new RectangleF(lefttop, size);
+            Color[] colors = {
+                Color.FromArgb(fromString("f7797d")),
+                Color.FromArgb(fromString("c6ffdd")),
+                Color.FromArgb(fromString("12c2e9")),
+                Color.FromArgb(fromString("f64f59")),
+                Color.FromArgb(fromString("96c93d")),
+                Color.FromArgb(fromString("00b09b")),
+                Color.FromArgb(fromString("7f7fd5")),
+                Color.FromArgb(fromString("91eae4")),
+                Color.FromArgb(fromString("544a7d")),
+                Color.FromArgb(fromString("ffd452")),
+                Color.FromArgb(fromString("a8ff78")),
+                Color.FromArgb(fromString("78ffd6"))
+            };
+            DateTime dt = DateTime.Now;
+            Random rnd = new Random();
+            Bitmap bmp = new Bitmap(this.panel1.Width, this.panel1.Height);
+            Graphics g = Graphics.FromImage(bmp);
+
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(rect);
+            PathGradientBrush pthGrBrush = new PathGradientBrush(path);
+            pthGrBrush.CenterColor = colors[mode*2];
+            Color[] surroundColors = { colors[mode * 2 + 1] };
+            pthGrBrush.SurroundColors = surroundColors;
+
+            Pen Phour = new Pen(Color.Black, 6);
+            Pen Pmin = new Pen(Color.Black, 4);
+            Pen Psec = new Pen(Color.Black, 2);
+            g.FillEllipse(pthGrBrush, rect);
+
+            int fontSize = (int)(len / 30);
+            PointF Pstr;
+            PointF PstrCenter = new PointF(pcenter.X - fontSize, pcenter.Y - 2 * fontSize);
+            SolidBrush brush = new SolidBrush(Color.Black);
+            for (int i = 1; i <= 12; i++)
+            {
+                Pstr = fromRtheta(PstrCenter, len / 2 * 0.87f, i > 9 ? i * 30 - 4 : i * 30);
+                g.DrawString(i.ToString(), new Font("微软雅黑", fontSize), brush, Pstr);
+            }
+            
+            g.DrawLine(Phour, pcenter, fromRtheta(pcenter, len / 2 * 0.55f, (dt.Hour % 12) * 30 + dt.Minute / 2f));
+            g.DrawLine(Pmin, pcenter, fromRtheta(pcenter, len / 2 * 0.65f, dt.Minute * 6f));
+            g.DrawLine(Psec, pcenter, fromRtheta(pcenter, len / 2 * 0.75f, dt.Second * 6f));
+            e.Graphics.DrawImage(bmp, 0, 0);
+
+            Phour.Dispose();
+            Pmin.Dispose();
+            Psec.Dispose();
+            brush.Dispose();
+            path.Dispose();
+            bmp.Dispose();
+            pthGrBrush.Dispose();
+            g.Dispose();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.panel1.Invalidate();
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            mode = (mode + 1) % modemax;
+        }
+    }
+}
